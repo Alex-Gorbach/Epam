018cc7df19052fdf0f5c5ca86e93b87f1953ac3d
Alex-Gorbach <alex_gorbach@inbox.ru>
2017-10-26 00:12:27 +0300

    Login to GitHub test.

---
+++ WebGitHubTest/WebGitHubTest/WebTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+
+namespace WebGitHubTest
+{
+    [TestFixture]
+    public class WebTest
+    {
+        FirefoxDriver driver;
+        [Test]
+        public void OneCanLogGitHub()
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://github.com/");
+            driver.FindElementByXPath(".//a[text() = 'Sign in']").Click();
+            driver.FindElementById("login_field").SendKeys("testautomationuser");
+            driver.FindElementById("password").SendKeys("Time4Death!");
+            driver.FindElementByName("commit").Click();
+
+        }
+    }
+}
